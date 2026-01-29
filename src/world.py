9048e389e6c35af92eb3571ac74a46852a4fe4c3
icebear104 <willy@gmail.com>
2026-01-30 07:28:17 +0800

    牆壁合併 修復子彈穿牆問題

--- src/world.py
+++ src/world.py
@@ -5,6 +5,7 @@ class World:
     def __init__(self,tmx_path):
         self.tmx_map=pytmx.load_pygame(tmx_path)
         self.collision_rects=[]
+        self.collision_rects_sep=[]
         self.mapSurface=None
         self.laod_collision()
         self.build_map()
@@ -13,7 +14,8 @@ class World:
         for x,y,gid in collision_layer:
             if gid:
                 rect=pygame.Rect(32*x-32,32*y-32,32,32)
-                self.collision_rects.append(rect) 
+                self.collision_rects_sep.append(rect) 
+        self.collide_rect_merge()
     def build_map(self):
         width=self.tmx_map.width*self.tmx_map.tilewidth
         height=self.tmx_map.height*self.tmx_map.tileheight
@@ -27,5 +29,30 @@ class World:
                     if gid:
                         tile=self.tmx_map.get_tile_image_by_gid(gid)
                         self.mapSurface.blit(tile,(32*x-32,32*y-32))
+    def collide_rect_merge(self):
+        self.merged_w=[]
+        self.rects=sorted(self.collision_rects_sep,key=lambda r:(r.y,r.x))
+        for r in self.rects:
+            if not self.merged_w:
+                self.merged_w.append(r)
+                continue
+            last=self.merged_w[-1]
+            if r.x==last.right and r.y == last.y :
+                last.width+=r.width
+            else:
+                self.merged_w.append(r)
+
+        self.merged_h=[]
+        self.rects=sorted(self.merged_w,key=lambda r:(r.x,r.y))
+        for r in self.rects:
+            if not self.merged_h:
+                self.merged_h.append(r)
+                continue
+            last=self.merged_h[-1]
+            if r.x==last.x and r.y == last.bottom and r.width==last.width:
+                last.height+=r.height
+            else:
+                self.merged_h.append(r)
+        self.collision_rects=self.merged_h
     def draw(self,surface):
         surface.blit(self.mapSurface,(0,0))
